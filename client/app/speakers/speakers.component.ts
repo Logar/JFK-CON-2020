63c68facd3a8e1426204eb503a30a8a2baf1909a
Ross Arena <rossarena89@gmail.com>
2020-10-11 18:18:21 -0600

    Adds hardcoded logic for speakers until a CMS is built

--- client/app/speakers/speakers.component.ts
+++ client/app/speakers/speakers.component.ts
@@ -7,9 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SpeakersComponent implements OnInit {
 
-  constructor() { }
+  primarySpeakers: Array<Object>;
+  secondarySpeakers: Array<Object>;
+
+  constructor() { 
+    this.primarySpeakers = [];
+    this.secondarySpeakers = [];
+  }
 
   ngOnInit(): void {
+    this.primarySpeakers.push({
+        thumbnail: 'jim-dieugenio.jpg',
+        name: 'Jim DiEugenio'
+      },
+      {
+        thumbnail: 'bill-simpich.jpg',
+        name: 'Bill Simpich'
+      },
+      {
+        thumbnail: 'robert-groden.jpg',
+        name: 'Robert Groden'
+      }
+    );
+    for (let count = 0; count < 11; count++) {
+      this.secondarySpeakers.push({
+        thumbnail: 'avatar.jpg',
+        name: 'TBA'
+      });
+    }
   }
 
 }
