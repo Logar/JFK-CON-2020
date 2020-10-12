30c69bb3c9906c8eb148cf29391bafeefa874974
Ross Arena <rossarena89@gmail.com>
2020-10-11 18:29:08 -0600

    Adds routing for speakers page and updates links to speakers page

--- client/app/app-routing.module.ts
+++ client/app/app-routing.module.ts
@@ -11,6 +11,7 @@ import { LoginComponent } from './login/login.component';
 import { LogoutComponent } from './logout/logout.component';
 import { AccountComponent } from './account/account.component';
 import { TicketsComponent } from './tickets/tickets.component';
+import { SpeakersComponent } from './speakers/speakers.component';
 import { AdminComponent } from './admin/admin.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
@@ -18,6 +19,7 @@ const routes: Routes = [
   { path: '', component: SplashComponent },
   { path: 'tickets', component: TicketsComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'speakers', component: SpeakersComponent },
   { path: 'login', component: LoginComponent },
   { path: 'logout', component: LogoutComponent },
   { path: 'account', component: AccountComponent, canActivate: [AuthGuardLogin] },
