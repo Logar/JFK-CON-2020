162151369f4c171efb4ff568e3c812506576f2c9
Ross Arena <rossarena89@gmail.com>
2020-10-18 03:21:59 -0600

    Extends MetaAbstract class and adds SEO routing

--- client/app/speakers/speakers.component.ts
+++ client/app/speakers/speakers.component.ts
@@ -1,16 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Router } from '@angular/router';
+
+import { MetaAbstract } from '../shared/abstract/meta.abstract'; 
+import { SEOService } from '../services/seo.service';
+
 @Component({
   selector: 'app-speakers',
   templateUrl: './speakers.component.html',
   styleUrls: ['./speakers.component.scss']
 })
-export class SpeakersComponent implements OnInit {
+export class SpeakersComponent extends MetaAbstract implements OnInit {
 
   primarySpeakers: Array<Object>;
   secondarySpeakers: Array<Object>;
 
-  constructor() { 
+  public constructor(
+    protected _seoService: SEOService,
+    protected _router: Router
+  ) {
+    // Invoke parent class constructor
+    super(_seoService, _router);
+
     this.primarySpeakers = [];
     this.secondarySpeakers = [];
   }
