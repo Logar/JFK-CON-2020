2cf725a3ad222f3eccc0a0ad472d5b6444b91019
Ross Arena <rossarena89@gmail.com>
2020-10-22 22:10:51 -0600

    Replaces static speaker data with methods for getting json from `speakers` collection

--- client/app/speakers/speakers.component.ts
+++ client/app/speakers/speakers.component.ts
@@ -1,51 +1,84 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
-import { MetaAbstract } from '../shared/abstract/meta.abstract'; 
+import { MetaAbstract } from '../shared/abstract/meta.abstract';
 import { SEOService } from '../services/seo.service';
+import { SpeakerService } from '../services/speaker.service';
+
+import { Speaker } from '../shared/models/speaker.model';
 
 @Component({
   selector: 'app-speakers',
   templateUrl: './speakers.component.html',
-  styleUrls: ['./speakers.component.scss']
+  styleUrls: ['./speakers.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class SpeakersComponent extends MetaAbstract implements OnInit {
 
   primarySpeakers: Array<Object>;
   secondarySpeakers: Array<Object>;
+  allSpeakers: Boolean;
+  speakerModel: Speaker;
 
   public constructor(
+    public speakerService: SpeakerService,
     protected _seoService: SEOService,
-    protected _router: Router
+    protected _router: Router,
+    private _route: ActivatedRoute
   ) {
     // Invoke parent class constructor
     super(_seoService, _router);
-
+    
     this.primarySpeakers = [];
     this.secondarySpeakers = [];
+    this.speakerModel = new Speaker();
+
+    this._route.params.subscribe(params => {
+
+      if (params['link']) {  
+        let speaker = Object.assign(
+          this.speakerModel,
+          {link: params['link']}
+        );
+        this.getSpeaker(speaker);
+      } else {
+        this.allSpeakers = true;
+        this.getAllSpeakers();
+      }
+    });
   }
 
-  ngOnInit(): void {
-    this.primarySpeakers.push({
-        thumbnail: 'jim-dieugenio.jpg',
-        name: 'Jim DiEugenio'
+  ngOnInit(): void { }
+
+  public getAllSpeakers(): void {
+    this.speakerService.getSpeakers().subscribe({
+      next: response => {
+        this.setAllSpeakers.call(this, response);
       },
-      {
-        thumbnail: 'bill-simpich.jpg',
-        name: 'Bill Simpich'
+      error: error => console.error('HTTP Error: ', error),
+      complete: () => console.log()
+    });
+  }
+
+  public getSpeaker(speaker: Speaker): void {
+    this.speakerService.getSpeaker(speaker).subscribe({
+      next: response => {
+        this.setSpeaker.call(this, response);
       },
-      {
-        thumbnail: 'robert-groden.jpg',
-        name: 'Robert Groden'
-      }
-    );
-    for (let count = 0; count < 11; count++) {
-      this.secondarySpeakers.push({
-        thumbnail: 'avatar.jpg',
-        name: 'TBA'
-      });
-    }
+      error: error => console.error('HTTP Error: ', error),
+      complete: () => console.log()
+    });
+  }
+
+  public setSpeaker(httpData: Array<Object>): void {
+    this.speakerModel = Object.assign(httpData);
+  }
+
+  public setAllSpeakers(httpData: Array<Object>): void {
+    console.log(httpData);
+    this.primarySpeakers = httpData.filter(item => item['primary'] === true);
+    this.secondarySpeakers = httpData.filter(item => item['primary'] === false);
   }
 
 }
