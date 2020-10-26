b896ef3b72ee65a42411f7dcce8e7b2dda3e5d6d
Ross Arena <rossarena89@gmail.com>
2020-10-26 01:15:07 -0600

    Adds MobileDetect for a readaptive / responsive approach to setting media breakpoints depending on device type

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,42 @@
-import { AfterViewChecked, ChangeDetectorRef, Component, ViewEncapsulation } from '@angular/core';
+import {
+  AfterViewChecked,
+  ChangeDetectorRef,
+  Component,
+  ViewEncapsulation,
+  OnInit,
+  Renderer2,
+  ElementRef
+} from '@angular/core';
+
 import { AuthService } from './services/auth.service';
+import MobileDetect from 'node_modules/mobile-detect/mobile-detect';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   encapsulation: ViewEncapsulation.None
 })
-export class AppComponent implements AfterViewChecked {
+export class AppComponent implements AfterViewChecked, OnInit {
 
-  constructor(public auth: AuthService,
-              private changeDetector: ChangeDetectorRef) { }
+  constructor(public _auth: AuthService,
+              private _changeDetector: ChangeDetectorRef,
+              private _renderer: Renderer2,
+              private _el: ElementRef) { }
 
   ngAfterViewChecked() {
-    this.changeDetector.detectChanges();
+    this._changeDetector.detectChanges();
   }
 
+  ngOnInit(): void {
+
+    let mobileDetect = new MobileDetect(window.navigator.userAgent);
+    
+    if (mobileDetect.phone()) {
+      this._renderer.addClass(this._el.nativeElement.firstChild, 'app-sm');
+    } else if (mobileDetect.tablet()) {
+      this._renderer.addClass(this._el.nativeElement.firstChild, 'app-lg');
+    } else {
+      this._renderer.addClass(this._el.nativeElement.firstChild, 'app-lg');
+    }
+  }
 }
