da7351180083f39dec0809563d398954e4d1e8a8
Ross Arena <rossarena89@gmail.com>
2020-11-07 01:26:18 -0700

    Creates <app-video-archive> component

---
+++ src/app/video-archive/video-archive.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-video-archive',
+  templateUrl: './video-archive.component.html',
+  styleUrls: ['./video-archive.component.scss']
+})
+export class VideoArchiveComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
