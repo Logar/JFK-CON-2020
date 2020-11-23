1ede3156027f200d4f0235c4949efcb93cd5e24d
Ross Arena <rossarena89@gmail.com>
2020-11-22 19:29:01 -0700

    Adds new tokens for separating between "All access" and "Limited access" conference attendees

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -18,7 +18,7 @@ export class AuthService {
 
   constructor(private userService: UserService,
               private jwtHelper: JwtHelperService) {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem('jfk-lancer-token');
     if (token) {
       const decodedUser = this.decodeUserFromToken(token);
       this.setCurrentUser(decodedUser);
@@ -27,26 +27,30 @@ export class AuthService {
 
   login(loginForm) {
     let input = loginForm.ticket.trim();
-    let found = tickets['default'].filter(item => item.number === input);
+    const ticketIndex = tickets['default'].findIndex(item => item.number === input);
+   
+    if (ticketIndex > -1) {
+      let rawToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJBc3Nhc3NpbmF0aW9uIENvbmZlcmVuY2UiLCJpYXQiOjE2MDU5MzkxODcsImV4cCI6MTYwOTM5NTE4NywiYXVkIjoiYXNzYXNzaW5hdGlvbmNvbmZlcmVuY2UuY29tIiwic3ViIjoiIiwicm9sZSI6ImxpbWl0ZWQtYWNjZXNzIiwiX2lkIjoicXdlcnR5dWlvcGFzZGZnaGprbHp4Y3Zibm0xMjM0NTYifQ.e-e8IYsijjQHtOTph4SkHaccPgDX8Z5_XcsXs2iO_Ik';
 
-    if (found[0]) {
-      let rawToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'; 
+      if (ticketIndex <= 100) {
+        rawToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJBc3Nhc3NpbmF0aW9uIENvbmZlcmVuY2UiLCJpYXQiOjE2MDU5MzkxODcsImV4cCI6MTYwOTM5NTE4NywiYXVkIjoiYXNzYXNzaW5hdGlvbmNvbmZlcmVuY2UuY29tIiwic3ViIjoiIiwicm9sZSI6ImFsbC1hY2Nlc3MiLCJfaWQiOiJxd2VydHl1aW9wYXNkZmdoamtsenhjdmJubTEyMzQ1NiJ9.KXRi7nP4b-0x7oaF1pPIGGIHmMOXFlkYg0Y125mDzq4';
+      }
+
       const decodedToken = this.jwtHelper.decodeToken(rawToken);
-      
-      localStorage.setItem('token', rawToken);
-      const decodedUser = this.decodeUserFromToken(rawToken);
+      localStorage.setItem('jfk-lancer-token', rawToken);
+
       this.setCurrentUser(decodedToken);
     }
     return this.loggedIn;
   }
 
   logout() {
-    localStorage.removeItem('token');
+    localStorage.removeItem('jfk-lancer-token');
     this.loggedIn = false;
     this.isAdmin = false;
     this.currentUser = new User();
   }
-  
+
   decodeUserFromToken(token) {
     return this.jwtHelper.decodeToken(token);
   }
@@ -54,12 +58,14 @@ export class AuthService {
   setCurrentUser(decodedUser) {
     // User is logged in
     this.loggedIn = true;
-
-    this.currentUser._id = decodedUser._id;
-    this.currentUser.username = decodedUser.username;
-    this.currentUser.role = decodedUser.role;
-      decodedUser.role === 'admin' ? this.isAdmin = true : this.isAdmin = false;
-    delete decodedUser.role;
+    this.currentUser = {
+      _id: decodedUser._id,
+      iss: decodedUser.iss,
+      iat: decodedUser.iat,
+      exp: decodedUser.exp,
+      aud: decodedUser.aud,
+      role: decodedUser.role
+    };
   }
 
 }
