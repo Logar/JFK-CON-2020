6927735d55ad078ae82f2d9319bfc95f91921f86
Ross Arena <rossarena89@gmail.com>
2020-11-22 19:25:18 -0700

    Adds functionality for a video archive

--- src/app/video-archive/video-archive.component.ts
+++ src/app/video-archive/video-archive.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+
+import * as VIDEO_DATA from '../../assets/json/videos.json';
+import { Router } from '@angular/router';
+import { User } from '../shared/models/user.model';
 
 @Component({
   selector: 'app-video-archive',
   templateUrl: './video-archive.component.html',
-  styleUrls: ['./video-archive.component.scss']
+  styleUrls: ['./video-archive.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class VideoArchiveComponent implements OnInit {
 
-  constructor() { }
+  videos: Array<Object>;
+  currentUser: User;
+  selectedDay: string;
 
-  ngOnInit(): void {
+  constructor(
+    protected _router: Router,
+    private _auth: AuthService) { 
+    this.videos = VIDEO_DATA['default'].Friday;
+    this.currentUser = this._auth.currentUser;
+    this.selectedDay = 'Friday';
+  }
+
+  ngOnInit(): void { 
+    if (!this._auth.loggedIn) {
+      this._router.navigate(['/']);
+    }
+  }
+
+  onDayChanged(day) {
+    this.selectedDay = day;
+    this.videos = VIDEO_DATA['default'][day];
   }
 
 }
